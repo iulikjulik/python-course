f5fb4a86be2bef912666906bce45a8bb11235e41
iulikjulik <145929749+iulikjulik@users.noreply.github.com>
2025-01-10 23:27:00 +0200

    Add files via upload

--- prog.py
+++ prog.py
@@ -11,25 +11,46 @@ class Shapes:
         print('Current Shapes: ')
         for shape in self.shapelist:
             if isinstance(shape, Circle):
-                print(f'Circle: radius={shape.radius}, area={shape.area}, perimeter={shape.perimeter}')
+                print(f'Circle: name={shape.name} radius={shape.radius}, area={shape.area}, perimeter={shape.perimeter}')
             elif isinstance(shape, Triangle):
-                print(f'Triangle: area={shape.area}, perimeter={shape.perimeter}')
+                print(f'Triangle: name={shape.name} area={shape.area}, perimeter={shape.perimeter}')
             elif isinstance(shape, Rectangle):
-                print(f'Rectangle: area={shape.area}, perimeter={shape.perimeter}')
+                print(f'Rectangle: name={shape.name} area={shape.area}, perimeter={shape.perimeter}')
         print('--------------------------------')
+    
+    def create(shape_type, shape_name):
+        if shape_type == "circle":
+            return Circle(name=shape_name)
+        elif shape_type == "rectangle":
+            return Rectangle(name=shape_name)
+        elif shape_type == "triangle":
+            return Triangle(name=shape_name)
+        else: print("Unknown shape type.")
+        return None
 
 def main():
+    def create(shape_type, shape_name):
+        if shape_type == "circle":
+            return Circle(name=shape_name)
+        elif shape_type == "rectangle":
+            return Rectangle(name=shape_name)
+        elif shape_type == "triangle":
+            return Triangle(name=shape_name)
+        else: print("Unknown shape type.")
+        return None
+
     shapes = Shapes()
-
-    shape1 = Circle(int(input('circle1 radius=')))
-    shapes.add_shapes(shape1)
-    shape2 = Circle(int(input('circle2 radius=')))
-    shapes.add_shapes(shape2)
-    shape3 = Triangle(int(input('side1=')), int(input('side2=')), int(input('side3=')))
-    shapes.add_shapes(shape3)
-    shape4 = Rectangle(int(input('rect width=')), int(input('rect length=')))
-    shapes.add_shapes(shape4)
-
-
-    shapes.display_shapes()
+    while True:
+        command = input("Enter command (create, display, exit): ")
+        if command == "create":
+            shape_type = input("Enter shape type (circle, rectangle, triangle): ")
+            shape = create(shape_type)
+            if shape:
+                shapes.add_shapes(shape)
+        elif command == "display":
+            shapes.display_shapes()
+        elif command == "exit":
+            break
+        else:
+            print("Unknown command.")
 main()
